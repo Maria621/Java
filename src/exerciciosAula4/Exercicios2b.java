c842683f6bc678bba06256388ad11d42bd474dcb
Maria De Fátima <133269366+Maria621@users.noreply.github.com>
2023-05-29 19:53:28 -0300

    atualizando

--- src/exerciciosAula4/Exercicios2b.java
+++ src/exerciciosAula4/Exercicios2b.java
@@ -4,34 +4,36 @@ import java.util.Scanner;
 
 public class Exercicios2b {
 	public static void main(String[] args) {
-		
+
 		Scanner leia = new Scanner(System.in);
-	    double[][] notas = new double[10][4];
+		double[][] notas = new double[10][4];
 		double[] medias = new double[10];
 
-		  // Leitura das notas
-		
-	            for (int i = 0; i < 10; i++) {
-                System.out.println("Digite as notas dos alunos " + (i + 1) + ":");
-		        for (int j = 0; j < 4; j++) {
-		        System.out.print("Nota " + (j + 1) + ": ");
-		        notas[i][j] = leia.nextDouble(); } }
+		// Leitura das notas
 
-	      // Cálculo das médias
-	  
-		        for (int i = 0; i < 10; i++) 
-		        {   double soma = 0;
-		        for (int j = 0; j < 4; j++) 
-		        { soma += notas[i][j];   }
-		          medias[i] = soma / 4;  }
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Digite as notas dos alunos " + (i + 1) + ":");
+			for (int j = 0; j < 4; j++) {
+				System.out.print("Nota " + (j + 1) + ": ");
+				notas[i][j] = leia.nextDouble();
+			}
+		}
 
-         // Exibição das médias
-		        
-		        System.out.println("\nMédias dos alunos:");
-		        for (int i = 0; i < 10; i++) {
-		        System.out.printf("Alunos %d: %.1f\n", (i + 1), medias[i]);
+		// Cálculo das médias
+
+		for (int i = 0; i < 10; i++) {
+			double soma = 0;
+			for (int j = 0; j < 4; j++) {
+				soma += notas[i][j];
+			}
+			medias[i] = soma / 4;
+		}
+
+		// Exibição das médias
+
+		System.out.println("\nMédias dos alunos:");
+		for (int i = 0; i < 10; i++) {
+			System.out.printf("Alunos %d: %.1f\n", (i + 1), medias[i]);
+		}
 	}
-  }
 }
-
-
